22301443bbed3e7e18be715f26511b1b0755bb77
JhoanTovar <tovarjhoan82sf@gmail.com>
2025-02-08 12:50:49 -0500

    primer commit con las clases necesarias vacias

---
+++ BlackProyectPractice/src/main/java/com/BlackProyectPackage/Controller.java
@@ -0,0 +1,4 @@
+package com.BlackProyectPackage;
+
+public class Controller {
+}
---
+++ BlackProyectPractice/src/main/java/com/BlackProyectPackage/Task.java
@@ -0,0 +1,7 @@
+package com.BlackProyectPackage;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
